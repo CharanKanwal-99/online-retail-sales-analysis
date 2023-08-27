09c83eb70d46fa4fd075ff30f7519df375528a59
Charan99 <111955279+CharanKanwal-99@users.noreply.github.com>
2023-08-28 04:25:54 +0530

    Update items.py

--- jumia/items.py
+++ jumia/items.py
@@ -8,5 +8,18 @@ import scrapy
 
 class JumiaItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    crawled_at = scrapy.Field()
+    name = scrapy.Field()
+    href = scrapy.Field()
+    data_id = scrapy.Field()
+    brand = scrapy.Field()
+    name2 = scrapy.Field()
+    price = scrapy.Field()
+    old_price = scrapy.Field()
+    discount = scrapy.Field()
+    votes = scrapy.Field()
+    stars = scrapy.Field()
+    image_url = scrapy.Field()
+    official_store = scrapy.Field()
+    inform = scrapy.Field()
